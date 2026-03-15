7ef305127eb3be7f046e754da7cd04db186ab132
rhawrami <ravanhawrami@gmail.com>
2026-03-15 16:47:53 -0500

    fix scalar move

--- pkg/op/avx2/broadcast.amd64.s
+++ pkg/op/avx2/broadcast.amd64.s
@@ -2,13 +2,13 @@
 
 #include "textflag.h"
 
-#define vBCast(vBrdCstOp, vMovOp, tMovOp, dSize, chnkSize) \
+#define vBCast(tReg, vBrdCstOp, vMovOp, tMovOp, dSize, chnkSize) \
     MOVQ dstAddr+0(FP), AX                                 \
     MOVQ dstLen+8(FP), CX                                  \
     MOVQ CX, SI                                            \
     XORQ DI, DI                                            \
     SUBQ chnkSize, SI                                      \
-    tMovOp lit+24(FP), R9                                  \
+    tMovOp lit+24(FP), tReg                                \
                                                            \
     TESTQ CX, CX                                           \
     JEQ exitFn                                             \
@@ -17,10 +17,10 @@
     JLT tradLoop                                           \
                                                            \
 vecLoop:                                                   \
-    vBrdCstOp R9, Y1                                       \
-    vBrdCstOp R9, Y2                                       \
-    vBrdCstOp R9, Y3                                       \
-    vBrdCstOp R9, Y4                                       \
+    vBrdCstOp tReg, Y1                                     \
+    vBrdCstOp tReg, Y2                                     \
+    vBrdCstOp tReg, Y3                                     \
+    vBrdCstOp tReg, Y4                                     \
     vMovOp Y1, (AX)                                        \
     vMovOp Y2, 32(AX)                                      \
     vMovOp Y3, 64(AX)                                      \
@@ -31,7 +31,7 @@ vecLoop:                                                   \
     JLT vecLoop                                            \
                                                            \
 tradLoop:                                                  \
-    tMovOp R9, (AX)                                        \
+    tMovOp tReg, (AX)                                      \
     ADDQ dSize, AX                                         \
     ADDQ $1, DI                                            \
     CMPQ DI, CX                                            \
@@ -42,20 +42,20 @@ exitFn:                                                    \
 
 // func broadcastB(dst []byte, lit byte)
 TEXT ·broadcastB(SB),NOSPLIT,$0-25
-    vBCast(VPBROADCASTB, VMOVDQU, MOVB, $1, $128)
+    vBCast(R9, VPBROADCASTB, VMOVDQU, MOVB, $1, $128)
 
 // func broadcastI64(dst []int64, lit int64)
 TEXT ·broadcastI64(SB),NOSPLIT,$0-32
-    vBCast(VPBROADCASTQ, VMOVDQU, MOVQ, $8, $16)
+    vBCast(R9, VPBROADCASTQ, VMOVDQU, MOVQ, $8, $16)
 
 // func broadcastI32(dst []int32, lit int32)
 TEXT ·broadcastI32(SB),NOSPLIT,$0-28
-    vBCast(VPBROADCASTD, VMOVDQU, MOVL, $4, $32)
+    vBCast(R9, VPBROADCASTD, VMOVDQU, MOVL, $4, $32)
 
 // func broadcastF64(dst []float64, lit float64)
 TEXT ·broadcastF64(SB),NOSPLIT,$0-32
-    vBCast(VBROADCASTSD, VMOVUPD, MOVQ, $8, $16)
+    vBCast(X0, VBROADCASTSD, VMOVUPD, VMOVSD, $8, $16)
 
 // func broadcastF32(dst []float32, lit float32)
 TEXT ·broadcastF32(SB),NOSPLIT,$0-28
-    vBCast(VBROADCASTSS, VMOVUPS, MOVL, $4, $32)
+    vBCast(X0, VBROADCASTSS, VMOVUPS, VMOVSS, $4, $32)
