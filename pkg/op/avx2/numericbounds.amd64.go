786f18cb6d0a0b2c20aea2de237af330a30b8ca0
r h <ravanhawrami@gmail.com>
2026-03-23 13:42:33 -0500

    add mmWValidity fn stubs

--- pkg/op/avx2/numericbounds.amd64.go
+++ pkg/op/avx2/numericbounds.amd64.go
@@ -37,3 +37,39 @@ func minmaxF64(src, dst []float64)
 
 //go:noescape
 func minmaxF32(src, dst []float32)
+
+//go:noescape
+func maxI64WithValidity(src, dst []int64, validity []byte)
+
+//go:noescape
+func minI64WithValidity(src, dst []int64, validity []byte)
+
+//go:noescape
+func maxI32WithValidity(src, dst []int32, validity []byte)
+
+//go:noescape
+func minI32WithValidity(src, dst []int32, validity []byte)
+
+//go:noescape
+func maxF64WithValidity(src, dst []float64, validity []byte)
+
+//go:noescape
+func minF64WithValidity(src, dst []float64, validity []byte)
+
+//go:noescape
+func maxF32WithValidity(src, dst []float32, validity []byte)
+
+//go:noescape
+func minF32WithValidity(src, dst []float32, validity []byte)
+
+//go:noescape
+func minmaxI64WithValidity(src, dst []int64, validity []byte)
+
+//go:noescape
+func minmaxI32WithValidity(src, dst []int32, validity []byte)
+
+//go:noescape
+func minmaxF64WithValidity(src, dst []float64, validity []byte)
+
+//go:noescape
+func minmaxF32WithValidity(src, dst []float32, validity []byte)
