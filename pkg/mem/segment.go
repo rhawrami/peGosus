c48e4078c7c113f2db2d1743094de5774fb5a388
r h <ravanhawrami@gmail.com>
2026-04-08 15:24:18 -0500

    trying new slab/bump/arena/region/whateverYouWantToCallIt Allocator

---
+++ pkg/mem/segment.go
@@ -0,0 +1,33 @@
+package mem
+
+import "sync/atomic"
+
+// Segment represents one piece of a slab.
+type Segment struct {
+	base     *byte        // base address
+	length   uint64       // byte length
+	capacity uint64       // maximum byte capacity
+	refCount atomic.Int64 // reference count
+	slab     *Slab        // slab that segment belongs to
+	reserved bool         // if false, can be returned/used by slab
+}
+
+// Inc increments the reference count by 1.
+func (s *Segment) Inc() {
+	s.refCount.Add(1)
+}
+
+// AsI64T casts `s` as a slice of 64-bit signed integers with length `l`.
+func (s *Segment) AsI64T(l int) []int64 { return asI64T(s.base, l) }
+
+// AsI32T casts `s` as a slice of 32-bit signed integers with length `l`.
+func (s *Segment) AsI32T(l int) []int32 { return asI32T(s.base, l) }
+
+// AsF64T casts `s` as a slice of 64-bit floating point values with length `l`.
+func (s *Segment) AsF64T(l int) []float64 { return asF64T(s.base, l) }
+
+// AsF32T casts `s` as a slice of 32-bit floating point values with length `l`.
+func (s *Segment) AsF32T(l int) []float32 { return asF32T(s.base, l) }
+
+// AsU32T casts `s` as a slice of 32-bit unsigned integers with length `l`.
+func (s *Segment) AsU32T(l int) []uint32 { return asU32T(s.base, l) }
---
+++ pkg/mem/unsafe.go
@@ -0,0 +1,27 @@
+package mem
+
+import "unsafe"
+
+func asBT(ptr *byte, length int) []byte {
+	return unsafe.Slice(ptr, length)
+}
+
+func asI64T(ptr *byte, length int) []int64 {
+	return unsafe.Slice((*int64)(unsafe.Pointer(ptr)), length)
+}
+
+func asI32T(ptr *byte, length int) []int32 {
+	return unsafe.Slice((*int32)(unsafe.Pointer(ptr)), length)
+}
+
+func asF64T(ptr *byte, length int) []float64 {
+	return unsafe.Slice((*float64)(unsafe.Pointer(ptr)), length)
+}
+
+func asF32T(ptr *byte, length int) []float32 {
+	return unsafe.Slice((*float32)(unsafe.Pointer(ptr)), length)
+}
+
+func asU32T(ptr *byte, length int) []uint32 {
+	return unsafe.Slice((*uint32)(unsafe.Pointer(ptr)), length)
+}
