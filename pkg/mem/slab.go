c121c199c4ce3a83f1b92f7e643aa4d687d65d04
r h <ravanhawrami@gmail.com>
2026-04-27 02:18:25 -0500

    werkin on slab stuff

--- pkg/mem/slab.go
+++ pkg/mem/slab.go
@@ -1,4 +1,127 @@
 package mem
 
+import "sync/atomic"
+
+const defaultSegL int = 10
+
+// MakeSlab generates a new Slab object with `l` bytes.
+func MakeSlab(l int) *Slab {
+	buff := makeAlignedSlice(l)
+	base := &buff[0]
+	segments := make([]*Segment, 0, defaultSegL)
+
+	s := &Slab{
+		buff:     buff,
+		base:     base,
+		used:     0,
+		capacity: uint64(l),
+		segments: segments,
+		nHoles:   0,
+	}
+	s.setUp()
+	return s
+}
+
+// Slab represents a contiguous byte buffer.
 type Slab struct {
+	buff     []byte     // underlying buffer
+	base     *byte      // base address
+	used     uint64     // currently used byte length
+	capacity uint64     // maximum byte capacity
+	segments []*Segment // set of segments
+	nHoles   uint64     // number of holes present
+}
+
+// setUp creates a single Segment belonging to `s`, with length
+// equal to the length of `s`.
+func (s *Slab) setUp() {
+	s.segments[0] = &Segment{
+		base:     s.base,
+		length:   s.capacity,
+		refCount: atomic.Int64{},
+		slab:     s,
+	}
+}
+
+// update updates `s`'s metadata, given another `l` bytes being used;
+// update is only meant to be used when `s` has no holes.
+func (s *Slab) update(l, o uint64) {
+	if s.nHoles > 0 {
+		return
+	}
+
+	s.used += l
+	seg := &Segment{
+		base:     &s.buff[o],
+		length:   0,
+		capacity: s.capacity - s.used,
+		refCount: atomic.Int64{},
+		slab:     s,
+	}
+	s.segments = append(s.segments, seg)
+}
+
+// coalesce attempts to coalesce free adjacent segments
+// into one; returns true if at least one coalescence succeeded;
+// for now, only two contiguous segments can be coalesced.
+func (s *Slab) coalesce() bool {
+	var yay bool
+
+	// # segments can change during loop
+	l := len(s.segments)
+	for i := 0; i < l; i++ {
+		if s.segments[i].refCount.Load() == 0 {
+			if s.segments[i+1].refCount.Load() == 0 {
+				a := s.segments[i]
+				b := s.segments[i+1]
+				a.capacity += b.capacity
+
+				// shift down by one, except final segment
+				for j := i + 1; j < len(s.segments)-1; j++ {
+					s.segments[j] = s.segments[j+1]
+				}
+				s.segments = s.segments[:len(s.segments)-1]
+				s.nHoles -= 2
+				l -= 1
+				yay = true
+			}
+		}
+	}
+	return yay
+}
+
+// NewSegment returns a Segment with at least `length` bytes; returns
+// (nil, false) if `s` cannot support a new segment with `length` bytes
+// in its current state.
+func (s *Slab) NewSegment(length int) (*Segment, bool) {
+	// ensure length divisible by alignment size
+	var l uint64
+	if l < uint64(alignSize) {
+		l = uint64(alignSize)
+	} else {
+		l = uint64(alignSize - (length & (alignSize - 1)) + length)
+	}
+
+	// works despite holes being present
+	if l <= s.segments[len(s.segments)-1].capacity {
+		seg := s.segments[len(s.segments)-1]
+		seg.length = uint64(length)
+		seg.refCount.Store(1)
+
+		s.update(l, (l + s.used))
+		return seg, true
+	}
+
+	if s.nHoles != 0 {
+		for _, v := range s.segments {
+			if v.refCount.Load() == 0 && v.capacity <= l {
+				v.length = l
+				v.refCount.Store(1)
+
+				s.nHoles -= 1 // one less hole
+				return v, true
+			}
+		}
+	}
+	return nil, false
 }
--- pkg/mem/unsafe.go
+++ pkg/mem/unsafe.go
@@ -2,8 +2,26 @@ package mem
 
 import "unsafe"
 
+const alignSize int = 64
+
+func makeAlignedSlice(l int) []byte {
+	if l <= 0 || l%alignSize != 0 {
+		return nil
+	}
+
+	s := make([]byte, l+alignSize)
+	offBy := int(uintptr(unsafe.Pointer(&s[0])) & uintptr(alignSize-1))
+	start := 0
+	if offBy != 0 {
+		start = 64 - offBy
+	}
+
+	s = s[start : start+l]
+	return s
+}
+
 func isAligned(addr *byte, a int) bool {
-	return uintptr(unsafe.Pointer(addr))&uintptr(a) == 0
+	return uintptr(unsafe.Pointer(addr))&uintptr(a-1) == 0
 }
 
 func asBT(ptr *byte, length int) []byte {
