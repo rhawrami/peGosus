6fe620d8a0d06c041a86b8a56e9e9e32ff45b18e
r h <ravanhawrami@gmail.com>
2026-04-30 23:03:33 -0500

    add alloc fns and sigs; i dont like this; need to think harder

--- pkg/mem/allocator.go
+++ pkg/mem/allocator.go
@@ -2,6 +2,12 @@ package mem
 
 const pawnSegmentSize int = 64
 
+// MakePawner returns a Pawner with an underlying slab of `l` bytes.
+func MakePawner(l int) *Pawner {
+	s := MakeSlab(l)
+	return &Pawner{s: s}
+}
+
 // Pawner represents a slab that only allocates segments of
 // length 64 bytes. Segments belonging to a Pawner should be used for small,
 // short-lived data. Coalescing will generally be avoided, and full-buffer clearing
@@ -111,12 +117,6 @@ func MakeAllocatorWithConfig(cfg AllocConfig) *Allocator {
 	}
 }
 
-// MakePawner returns a Pawner with an underlying slab of `l` bytes.
-func MakePawner(l int) *Pawner {
-	s := MakeSlab(l)
-	return &Pawner{s: s}
-}
-
 // Allocator "allocates" and manages memory.
 type Allocator struct {
 	on       uint64   // offset into slabs slice
@@ -127,8 +127,64 @@ type Allocator struct {
 	scratch  *Scratch // slab for temporary objects
 }
 
+// Update updates `a`'s metadata; returns true if the metadata changed.
+func (a *Allocator) Update() bool {
+	var u0, c0, u1, c1 uint64
+	u0 = a.used
+	c0 = a.capacity
+	for _, v := range a.slabs {
+		u1 += v.used
+		c1 += v.capacity
+	}
+	a.used = u1
+	a.capacity = c1
+	return (u0 != u1) || (c0 != c1)
+}
+
 // PawnerIsValid returns true if `a`'s Pawner object is non-nil.
 func (a *Allocator) PawnerIsValid() bool { return a.pawner != nil }
 
 // ScratchIsValid returns true if `a`'s Scratch object is non-nil.
 func (a *Allocator) ScratchIsValid() bool { return a.scratch != nil }
+
+// AllocTiny returns a 64-byte Data object; returns false if cannot
+// allocate.
+func (a *Allocator) AllocTiny() (*Data, bool) {
+	if a.PawnerIsValid() {
+		if s, ok := a.pawner.MakeSegment(); ok {
+			return MakeData([]*Segment{s}), true
+		}
+	}
+	return nil, false
+}
+
+// AllocTemp allocates a Data object that should be temporary.
+func (a *Allocator) AllocTemp(l int) (*Data, bool) {
+	if a.ScratchIsValid() {
+		if s, ok := a.scratch.MakeSegment(l); ok {
+			return MakeData([]*Segment{s}), true
+		}
+	}
+	return nil, false
+}
+
+// Alloc returns a Data object with a single segment of
+// at least `l` bytes.
+// TODO
+func (a *Allocator) Alloc(l int) (*Data, bool)
+
+// AllocWithProfile returns a Data object matching the size profile `s`;
+// in other words, the Data object will have len(`s`.p) segments, each with
+// at least `s`.p[i] bytes of capacity.
+// TODO
+func (a *Allocator) AllocWithProfile(s SizeProfile) (*Data, bool)
+
+// AllocContiguous returns a Data object with segments all coming from
+// the same slab, and all contiguous to each other. This allows for
+// easier coalescing later on.
+// TODO
+func (a *Allocator) AllocContiguous(s SizeProfile) (*Data, bool)
+
+// Grow adds another slab with at least `l` bytes to the allocator.
+// TODO
+func (a *Allocator) Grow(l int)
