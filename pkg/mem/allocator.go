ec9e7ca3eae3180c6f235d861bcf286ec008e3ad
r h <ravanhawrami@gmail.com>
2026-05-03 19:41:46 -0500

    move pawn/scratch to other files; add Grow fn

--- pkg/mem/allocator.go
+++ pkg/mem/allocator.go
@@ -1,72 +1,13 @@
 package mem
 
-const pawnSegmentSize int = 64
+// AllocLocation represents where a segment belongs to.
+type AllocLocation int
 
-// MakePawner returns a Pawner with an underlying slab of `l` bytes.
-func MakePawner(l int) *Pawner {
-	s := MakeSlab(l)
-	return &Pawner{s: s}
-}
-
-// Pawner represents a slab that only allocates segments of
-// length 64 bytes. Segments belonging to a Pawner should be used for small,
-// short-lived data. Coalescing will generally be avoided, and full-buffer clearing
-// will be preferred. In the future, Pawner will be its own complete type, and Slab/Pawner
-// will share some interface.
-type Pawner struct {
-	s *Slab
-}
-
-// Clear gives `p` a fresh slate.
-func (p *Pawner) Clear() {
-	p.s.Clear()
-}
-
-// MakeSegment returns a 64-byte segment.
-func (p *Pawner) MakeSegment() (*Segment, bool) {
-	return p.s.MakeSegment(pawnSegmentSize)
-}
-
-// TakeSegment takes a 64-byte segment.
-func (p *Pawner) TakeSegment(g *Segment) {
-	// remove this panic after testing.
-	if g.capacity != uint64(pawnSegmentSize) {
-		panic("TakeSegment: Pawner can only take segments of capacity 64")
-	}
-	p.s.TakeSegment(g)
-}
-
-// MakeScratch returns a Scratch object with at least
-// `l` bytes in its underlying slab.
-func MakeScratch(l int) *Scratch {
-	s := MakeSlab(l)
-	return &Scratch{s: s}
-}
-
-// Scratch represents a slab that should be used to hold temporary
-// objects. While `TakeSegment` is exposed, `Clear` should be the
-// primary function of reusing segments in a Scratch buffer. Generally,
-// Scratch segments should only live within a single function call.
-type Scratch struct {
-	s *Slab
-}
-
-// MakeSegment returns a segment with at least `l` bytes in length;
-// returns false if not enough space.
-func (c *Scratch) MakeSegment(l int) (*Segment, bool) {
-	return c.s.MakeSegment(l)
-}
-
-// Clear clears a Scratch object's buffer and segments
-func (c *Scratch) Clear() {
-	c.s.Clear()
-}
-
-// TakeSegment takes a segment belonging to the Scratch object; should
-// be used sparingly.
-func (c *Scratch) TakeSegment(g *Segment) {
-	c.s.TakeSegment(g)
-}
+const (
+	FromSlabs AllocLocation = iota
+	FromPawner
+	FromScratch
+)
 
 // MakeAllocConfig returns an AllocConfig object.
 func MakeAllocConfig(slabProfile []uint64, pawnerSize, scratchSize uint64) AllocConfig {
@@ -77,14 +18,15 @@ func MakeAllocConfig(slabProfile []uint64, pawnerSize, scratchSize uint64) Alloc
 	}
 }
 
-// AllocConfig configurates an initialized Allocator; if pawnerSize (scratchSize)
+// AllocConfig configures an initialized Allocator; if pawnerSize (scratchSize)
 // is 0, then the underlying Pawner (Scratch) pointer will be nil.
 type AllocConfig struct {
 	slabProfile []uint64 // byte capacity per slab
-	pawnerSize  uint64   // pawner byte byte capacity
-	scratchSize uint64
+	pawnerSize  uint64   // pawner byte capacity
+	scratchSize uint64   // scratch byte capacity
 }
 
+// MakeAllocatorWithConfig returns an Allocator based on `cfg`.
 func MakeAllocatorWithConfig(cfg AllocConfig) *Allocator {
 	var pawner *Pawner
 	var scratch *Scratch
@@ -117,14 +59,21 @@ func MakeAllocatorWithConfig(cfg AllocConfig) *Allocator {
 	}
 }
 
-// Allocator "allocates" and manages memory.
+// Allocator allocates and manages memory.
 type Allocator struct {
 	on       uint64   // offset into slabs slice
 	used     uint64   // bytes currently used FOR SLABS
 	capacity uint64   // maximum byte capacity FOR SLABS
 	slabs    []*Slab  // slabs
-	pawner   *Pawner  // slab with fixed 64-byte segments
+	pawner   *Pawner  // slab with fixed 128-byte segments
 	scratch  *Scratch // slab for temporary objects
+}
+
+// Grow adds another slab with at least `l` bytes to the allocator.
+func (a *Allocator) Grow(l int) {
+	s := MakeSlab(l)
+	a.slabs = append(a.slabs, s)
+	a.capacity += s.capacity
 }
 
 // Update updates `a`'s metadata; returns true if the metadata changed.
@@ -147,26 +96,14 @@ func (a *Allocator) PawnerIsValid() bool { return a.pawner != nil }
 // ScratchIsValid returns true if `a`'s Scratch object is non-nil.
 func (a *Allocator) ScratchIsValid() bool { return a.scratch != nil }
 
-// AllocTiny returns a 64-byte Data object; returns false if cannot
-// allocate.
-func (a *Allocator) AllocTiny() (*Data, bool) {
-	if a.PawnerIsValid() {
-		if s, ok := a.pawner.MakeSegment(); ok {
-			return MakeData([]*Segment{s}), true
-		}
-	}
-	return nil, false
-}
+// AllocTiny returns a 128-byte Data object, and where it was allocated from.
+// TODO
+func (a *Allocator) AllocTiny() (*Data, AllocLocation)
 
-// AllocTemp allocates a Data object that should be temporary.
-func (a *Allocator) AllocTemp(l int) (*Data, bool) {
-	if a.ScratchIsValid() {
-		if s, ok := a.scratch.MakeSegment(l); ok {
-			return MakeData([]*Segment{s}), true
-		}
-	}
-	return nil, false
-}
+// AllocTemp allocates a Data object that should be temporary,
+// and where it was allocated from.
+// TODO
+func (a *Allocator) AllocTemp(l int) (*Data, AllocLocation)
 
 // Alloc returns a Data object with a single segment of
 // at least `l` bytes.
@@ -184,7 +121,3 @@ func (a *Allocator) AllocWithProfile(s SizeProfile) (*Data, bool)
 // easier coalescing later on.
 // TODO
 func (a *Allocator) AllocContiguous(s SizeProfile) (*Data, bool)
-
-// Grow adds another slab with at least `l` bytes to the allocator.
-// TODO
-func (a *Allocator) Grow(l int)
