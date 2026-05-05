c44ea1d5f91e8382dc2468f1c04647b03da018a9
r h <ravanhawrami@gmail.com>
2026-05-05 17:26:02 -0500

    add allocation statistics, dont expose allocloc

--- pkg/mem/allocator.go
+++ pkg/mem/allocator.go
@@ -1,14 +1,5 @@
 package mem
 
-// AllocLoc represents where a segment belongs to.
-type AllocLoc int
-
-const (
-	FromSlabs AllocLoc = iota
-	FromPawner
-	FromScratch
-)
-
 // MakeAllocConfig returns an AllocConfig object.
 func MakeAllocConfig(slabProfile []uint64, pawnerSize, scratchSize uint64) AllocConfig {
 	return AllocConfig{
@@ -50,6 +41,7 @@ func MakeAllocatorWithConfig(cfg AllocConfig) *Allocator {
 	}
 
 	return &Allocator{
+		stats:    &AllocStats{},
 		on:       0,
 		capacity: capacity,
 		slabs:    slabs,
@@ -58,13 +50,53 @@ func MakeAllocatorWithConfig(cfg AllocConfig) *Allocator {
 	}
 }
 
+// allocLoc represents where a segment belongs to.
+type allocLoc int
+
+const (
+	fromSlabs allocLoc = iota
+	fromPawner
+	fromScratch
+)
+
+// AllocStats tracks allocation statistics.
+type AllocStats struct {
+	lastOptimized uint64    // number of allocations since last optimization
+	avgReq        uint64    // rounded average allocation request size
+	nLocs         [3]uint64 // number of allocations per slabs/pawner/scratch
+}
+
+// resetLastOptimized sets lastOptimized to zero.
+func (a *AllocStats) resetLastOptimized() {
+	a.lastOptimized = 0
+}
+
+// updateAvgReq updates the average request size.
+func (a *AllocStats) updateAvgReq(l int) {
+	a.avgReq = (a.avgReq + uint64(l)) >> 1
+}
+
+// updateNLocs updates the number of allocations for a given `l` location.
+func (a *AllocStats) updateNLocs(l allocLoc) {
+	a.nLocs[l] += 1
+}
+
+// updateState updates the average request size, and the number
+// of allocations-per-location; increments lastOptimized.
+func (a *AllocStats) updateState(n int, l allocLoc) {
+	a.lastOptimized += 1
+	a.updateAvgReq(n)
+	a.updateNLocs(l)
+}
+
 // Allocator allocates and manages memory.
 type Allocator struct {
-	on       uint64   // offset into slabs slice
-	capacity uint64   // maximum byte capacity FOR SLABS
-	slabs    []*Slab  // slabs
-	pawner   *Pawner  // slab with fixed 128-byte segments
-	scratch  *Scratch // slab for temporary objects
+	stats    *AllocStats // allocation statistics
+	on       uint64      // offset into slabs slice
+	capacity uint64      // maximum byte capacity FOR SLABS
+	slabs    []*Slab     // slabs
+	pawner   *Pawner     // slab with fixed 128-byte segments
+	scratch  *Scratch    // slab for temporary objects
 }
 
 // Clear clears all underlying slabs in `a`.
@@ -140,7 +172,7 @@ func (a *Allocator) allocSegFromCurrentSlabs(l int) (*Segment, bool) {
 
 // allocTinySegment returns a 128-byte segment, guaranteed to come from
 // the pawner slab.
-func (a *Allocator) allocTinySegment() (*Segment, AllocLoc) {
+func (a *Allocator) allocTinySegment() (*Segment, allocLoc) {
 	if !a.PawnerIsValid() {
 		// if a pawn is needed, then it's probably worth making a small pawner.
 		a.pawner = MakePawner(pawnSegmentSize * 5)
@@ -150,12 +182,12 @@ func (a *Allocator) allocTinySegment() (*Segment, AllocLoc) {
 	// b) tiny segments are more in use than expected
 	// so, the necessary copy and 1280 bytes added is fine.
 	g := a.pawner.MakeSegmentForce()
-	return g, FromPawner
+	return g, fromPawner
 }
 
 // allocTempSegment allocates a segment that should be temporary,
 // and where it was allocated from.
-func (a *Allocator) allocTempSegment(l int) (*Segment, AllocLoc) {
+func (a *Allocator) allocTempSegment(l int) (*Segment, allocLoc) {
 	// if small enough, just use the pawner
 	if l <= pawnSegmentSize {
 		return a.allocTinySegment()
@@ -170,61 +202,65 @@ func (a *Allocator) allocTempSegment(l int) (*Segment, AllocLoc) {
 
 	if a.ScratchIsValid() {
 		if g, ok := a.scratch.MakeSegment(l); ok {
-			return g, FromScratch
+			return g, fromScratch
 		}
 	}
 
 	// need to allocate from slabs now
 	if g, ok := a.allocSegFromCurrentSlabs(l); ok {
-		return g, FromSlabs
+		return g, fromSlabs
 	}
 	a.Grow(l + (l >> 1))
 	a.on = uint64(len(a.slabs) - 1)
 	// guaranteed to work
 	g, _ := a.slabs[a.on].MakeSegment(l)
-	return g, FromSlabs
+	return g, fromSlabs
 
 }
 
 // allocSegment returns a segment of at least `l` bytes, along
 // with where the data was allocated from.
-func (a *Allocator) allocSegment(l int) (*Segment, AllocLoc) {
+func (a *Allocator) allocSegment(l int) (*Segment, allocLoc) {
 	if l < pawnSegmentSize {
 		return a.allocTinySegment()
 	}
 
 	// need to allocate from slabs now
 	if g, ok := a.allocSegFromCurrentSlabs(l); ok {
-		return g, FromSlabs
+		return g, fromSlabs
 	}
 	a.Grow(l + (l >> 1))
 	a.on = uint64(len(a.slabs) - 1)
 	// guaranteed to work
 	g, _ := a.slabs[a.on].MakeSegment(l)
-	return g, FromSlabs
+	return g, fromSlabs
 }
 
 // AllocTemp returns a Data object with a single segment, with the
 // implication that the object is temporary and will be freed shortly.
-func (a *Allocator) AllocTemp(l int) (*Data, AllocLoc) {
+func (a *Allocator) AllocTemp(l int) *Data {
 	g, loc := a.allocTempSegment(l)
-	return MakeDataFromSingleSegment(g), loc
+	a.stats.updateState(l, loc)
+
+	return MakeDataFromSingleSegment(g)
 }
 
 // Alloc returns a Data object with a single segment of at least `l` bytes.
-func (a *Allocator) Alloc(l int) (*Data, AllocLoc) {
+func (a *Allocator) Alloc(l int) *Data {
 	g, loc := a.allocSegment(l)
-	return MakeDataFromSingleSegment(g), loc
+	a.stats.updateState(l, loc)
+	return MakeDataFromSingleSegment(g)
 }
 
 // AllocWithProfile returns a Data object matching the size profile `s`;
 // in other words, the Data object will have len(`s`.p) segments, each with
-// at least `s`.p[i] bytes of capacity; also returns slice of allocation locations
-func (a *Allocator) AllocWithProfile(s SizeProfile) (*Data, []AllocLoc) {
+// at least `s`.p[i] bytes of capacity.
+func (a *Allocator) AllocWithProfile(s SizeProfile) *Data {
 	segs := make([]*Segment, len(s.p))
-	locs := make([]AllocLoc, 0, len(s.p))
 	for i := 0; i < len(s.p); i++ {
-		segs[i], locs[i] = a.allocSegment(int(s.p[i]))
+		g, l := a.allocSegment(int(s.p[i]))
+		segs[i] = g
+		a.stats.updateState(int(s.p[i]), l)
 	}
-	return MakeData(segs), locs
+	return MakeData(segs)
 }
