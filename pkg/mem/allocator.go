08c7b77d327f6523da87b722a6763dc6efff4cad
r h <ravanhawrami@gmail.com>
2026-05-05 21:55:54 -0500

    simplify allocator; just general and scratch

--- pkg/mem/allocator.go
+++ pkg/mem/allocator.go
@@ -1,69 +1,44 @@
 package mem
 
 // MakeAllocConfig returns an AllocConfig object.
-func MakeAllocConfig(slabProfile []uint64, pawnerSize, scratchSize uint64) AllocConfig {
+func MakeAllocConfig(generalProfile, scratchProfile []uint64) AllocConfig {
 	return AllocConfig{
-		slabProfile: slabProfile,
-		pawnerSize:  pawnerSize,
-		scratchSize: scratchSize,
+		generalProfile: generalProfile,
+		scratchProfile: scratchProfile,
 	}
 }
 
-// AllocConfig configures an initialized Allocator; if pawnerSize (scratchSize)
-// is 0, then the underlying Pawner (Scratch) pointer will be nil.
+// AllocConfig configures an Allocator.
 type AllocConfig struct {
-	slabProfile []uint64 // byte capacity per slab
-	pawnerSize  uint64   // pawner byte capacity
-	scratchSize uint64   // scratch byte capacity
+	generalProfile []uint64 // byte capacity per general slab
+	scratchProfile []uint64 // byte capacity per scratch slab
 }
 
 // MakeAllocatorWithConfig returns an Allocator based on `cfg`.
 func MakeAllocatorWithConfig(cfg AllocConfig) *Allocator {
-	var pawner *Pawner
-	var scratch *Scratch
-	if cfg.pawnerSize == 0 {
-		pawner = nil
-	} else {
-		pawner = MakePawner(int(cfg.pawnerSize))
-	}
-	if cfg.scratchSize == 0 {
-		scratch = nil
-	} else {
-		scratch = MakeScratch(int(cfg.scratchSize))
-	}
-
-	slabs := make([]*Slab, len(cfg.slabProfile))
-	var capacity uint64
-	for i := 0; i < len(cfg.slabProfile); i++ {
-		s := MakeSlab(int(cfg.slabProfile[i]))
-		capacity += s.capacity
-		slabs[i] = s
-	}
+	general := MakeSlabSet(cfg.generalProfile)
+	scratch := MakeSlabSet(cfg.scratchProfile)
 
 	return &Allocator{
-		stats:    &AllocStats{},
-		on:       0,
-		capacity: capacity,
-		slabs:    slabs,
-		pawner:   pawner,
-		scratch:  scratch,
+		stats:   &AllocStats{},
+		general: general,
+		scratch: scratch,
 	}
 }
 
-// allocLoc represents where a segment belongs to.
-type allocLoc int
+// reqLoc represents where a segment was initially requested from.
+type reqLoc int
 
 const (
-	fromSlabs allocLoc = iota
-	fromPawner
-	fromScratch
+	reqGeneral reqLoc = iota
+	reqScratch
 )
 
-// AllocStats tracks allocation statistics.
+// AllocStats tracks allocation statistics; may be used later.
 type AllocStats struct {
 	lastOptimized uint64    // number of allocations since last optimization
 	avgReq        uint64    // rounded average allocation request size
-	nLocs         [3]uint64 // number of allocations per slabs/pawner/scratch
+	nLocs         [2]uint64 // number of allocations per general/scratch
 }
 
 // resetLastOptimized sets lastOptimized to zero.
@@ -73,17 +48,21 @@ func (a *AllocStats) resetLastOptimized() {
 
 // updateAvgReq updates the average request size.
 func (a *AllocStats) updateAvgReq(l int) {
-	a.avgReq = (a.avgReq + uint64(l)) >> 1
+	r := uint64(l)
+	if a.avgReq != 0 {
+		a.avgReq = (a.avgReq + uint64(l)) >> 1
+	}
+	a.avgReq = r
 }
 
 // updateNLocs updates the number of allocations for a given `l` location.
-func (a *AllocStats) updateNLocs(l allocLoc) {
+func (a *AllocStats) updateNLocs(l reqLoc) {
 	a.nLocs[l] += 1
 }
 
 // updateState updates the average request size, and the number
 // of allocations-per-location; increments lastOptimized.
-func (a *AllocStats) updateState(n int, l allocLoc) {
+func (a *AllocStats) updateState(n int, l reqLoc) {
 	a.lastOptimized += 1
 	a.updateAvgReq(n)
 	a.updateNLocs(l)
@@ -91,176 +70,105 @@ func (a *AllocStats) updateState(n int, l allocLoc) {
 
 // Allocator allocates and manages memory.
 type Allocator struct {
-	stats    *AllocStats // allocation statistics
-	on       uint64      // offset into slabs slice
-	capacity uint64      // maximum byte capacity FOR SLABS
-	slabs    []*Slab     // slabs
-	pawner   *Pawner     // slab with fixed 128-byte segments
-	scratch  *Scratch    // slab for temporary objects
+	stats   *AllocStats // allocation statistics
+	general *SlabSet    // set for general, non-temporary use
+	scratch *SlabSet    // set for temporary data
 }
 
-// Clear clears all underlying slabs in `a`.
-func (a *Allocator) Clear() {
-	for _, v := range a.slabs {
-		v.Clear()
-	}
-	a.on = 0
-
-	if a.PawnerIsValid() {
-		a.pawner.Clear()
-	}
-
-	if a.ScratchIsValid() {
-		a.scratch.Clear()
-	}
+// ClearGeneral clears the general slabs.
+func (a *Allocator) ClearGeneral() {
+	a.general.Clear()
 }
 
-// ClearSlabs clears all slabs in the slab set, excluding the
-// pawner and scratch.
-func (a *Allocator) ClearSlabs() {
-	for _, v := range a.slabs {
-		v.Clear()
-	}
-	a.on = 0
-}
-
-// ClearPawner clears the pawner slab, if non-nil.
-func (a *Allocator) ClearPawner() {
-	if a.PawnerIsValid() {
-		a.pawner.Clear()
-	}
-}
-
-// ClearScratch clears the scratch slab, if non-nil.
+// ClearScratch clears the scratch slabs.
 func (a *Allocator) ClearScratch() {
-	if a.ScratchIsValid() {
-		a.scratch.Clear()
-	}
+	a.scratch.Clear()
 }
 
-// Grow adds another slab with at least `l` bytes to the allocator.
-func (a *Allocator) Grow(l int) {
-	s := MakeSlab(l)
-	a.slabs = append(a.slabs, s)
-	a.capacity += s.capacity
+// Clear clears all underlying slabs.
+func (a *Allocator) Clear() {
+	a.ClearGeneral()
+	a.ClearScratch()
 }
 
-// PawnerIsValid returns true if `a`'s Pawner object is non-nil.
-func (a *Allocator) PawnerIsValid() bool { return a.pawner != nil }
-
-// ScratchIsValid returns true if `a`'s Scratch object is non-nil.
-func (a *Allocator) ScratchIsValid() bool { return a.scratch != nil }
-
-// allocSegFromCurrentSlabs attempts to make a segment, with at least `l` bytes,
-// from the current slabs in `a` (e.g., will not grow or add slabs); returns false
-// if unable to.
-func (a *Allocator) allocSegFromCurrentSlabs(l int) (*Segment, bool) {
-	if g, ok := a.slabs[a.on].MakeSegmentWithCoalesce(l); ok {
-		return g, true
-	}
-
-	for i, v := range a.slabs {
-		if i != int(a.on) {
-			if g, ok := v.MakeSegmentWithCoalesce(l); ok {
-				return g, true
-			}
-		}
-	}
-
-	return nil, false
+// GrowGeneral adds another slab with at least `l` bytes to the
+// general slab set.
+func (a *Allocator) GrowGeneral(l int) {
+	a.general.GrowWithSize(l)
 }
 
-// allocTinySegment returns a 128-byte segment, guaranteed to come from
-// the pawner slab.
-func (a *Allocator) allocTinySegment() (*Segment, allocLoc) {
-	if !a.PawnerIsValid() {
-		// if a pawn is needed, then it's probably worth making a small pawner.
-		a.pawner = MakePawner(pawnSegmentSize * 5)
-	}
-	// try pawner, and grow if necessary; if pawner is maxed out, that means
-	// a) pawner slab is too small, and/or
-	// b) tiny segments are more in use than expected
-	// so, the necessary copy and 1280 bytes added is fine.
-	g := a.pawner.MakeSegmentForce()
-	return g, fromPawner
-}
-
-// allocTempSegment allocates a segment that should be temporary,
-// and where it was allocated from.
-func (a *Allocator) allocTempSegment(l int) (*Segment, allocLoc) {
-	// if small enough, just use the pawner
-	if l <= pawnSegmentSize {
-		return a.allocTinySegment()
-	}
-
-	// if a scratch isn't valid, but this fn is being called,
-	// and if `l` isn't ridiculously large (relative to slabs),
-	// it's probably worth making a scratch slab (with a bit more space than needed)
-	if !a.ScratchIsValid() && (l*2) < int(a.slabs[0].capacity) {
-		a.scratch = MakeScratch(l + (l >> 1))
-	}
-
-	if a.ScratchIsValid() {
-		if g, ok := a.scratch.MakeSegment(l); ok {
-			return g, fromScratch
-		}
-	}
-
-	// need to allocate from slabs now
-	if g, ok := a.allocSegFromCurrentSlabs(l); ok {
-		return g, fromSlabs
-	}
-	a.Grow(l + (l >> 1))
-	a.on = uint64(len(a.slabs) - 1)
-	// guaranteed to work
-	g, _ := a.slabs[a.on].MakeSegment(l)
-	return g, fromSlabs
-
-}
-
-// allocSegment returns a segment of at least `l` bytes, along
-// with where the data was allocated from.
-func (a *Allocator) allocSegment(l int) (*Segment, allocLoc) {
-	if l < pawnSegmentSize {
-		return a.allocTinySegment()
-	}
-
-	// need to allocate from slabs now
-	if g, ok := a.allocSegFromCurrentSlabs(l); ok {
-		return g, fromSlabs
-	}
-	a.Grow(l + (l >> 1))
-	a.on = uint64(len(a.slabs) - 1)
-	// guaranteed to work
-	g, _ := a.slabs[a.on].MakeSegment(l)
-	return g, fromSlabs
+// GrowScratch adds another slab with at least `l` bytes to the
+// scratch slab set.
+func (a *Allocator) GrowScratch(l int) {
+	a.scratch.GrowWithSize(l)
 }
 
 // AllocTemp returns a Data object with a single segment, with the
 // implication that the object is temporary and will be freed shortly.
 func (a *Allocator) AllocTemp(l int) *Data {
-	g, loc := a.allocTempSegment(l)
-	a.stats.updateState(l, loc)
+	a.stats.updateState(l, reqScratch)
+	// check scratch, then general, then grow scratch
+	g, ok := a.scratch.MakeSegment(l)
+	if !ok {
+		g, ok = a.general.MakeSegment(l)
+	}
+	if !ok {
+		g = a.scratch.GrowAndMakeSegment(l)
+	}
 
 	return MakeDataFromSingleSegment(g)
 }
 
 // Alloc returns a Data object with a single segment of at least `l` bytes.
 func (a *Allocator) Alloc(l int) *Data {
-	g, loc := a.allocSegment(l)
-	a.stats.updateState(l, loc)
+	a.stats.updateState(l, reqGeneral)
+	g := a.general.ForceSegment(l)
+
 	return MakeDataFromSingleSegment(g)
 }
 
 // AllocWithProfile returns a Data object matching the size profile `s`;
 // in other words, the Data object will have len(`s`.p) segments, each with
 // at least `s`.p[i] bytes of capacity.
-func (a *Allocator) AllocWithProfile(s SizeProfile) *Data {
-	segs := make([]*Segment, len(s.p))
-	for i := 0; i < len(s.p); i++ {
-		g, l := a.allocSegment(int(s.p[i]))
-		segs[i] = g
-		a.stats.updateState(int(s.p[i]), l)
+func (a *Allocator) AllocWithProfile(p SizeProfile) *Data {
+	firstP := int(p.p[0])
+	a.stats.updateState(firstP, reqGeneral)
+
+	g := a.general.ForceSegment(firstP)
+	d := MakeDataFromSingleSegment(g)
+
+	for i := 1; i < len(p.p); i++ {
+		l := int(p.p[i])
+		a.stats.updateState(l, reqGeneral)
+
+		g = a.general.ForceSegment(l)
+		d.AddSegment(g, false)
 	}
+
+	return d
+}
+
+// AllocTempWithProfile returns a Data object matching the size profile `s`;
+// in other words, the Data object will have len(`s`.p) segments, each with
+// at least `s`.p[i] bytes of capacity; implied that the object is
+// temporary and will be freed shortly.
+func (a *Allocator) AllocTempWithProfile(p SizeProfile) *Data {
+	segs := make([]*Segment, len(p.p))
+
+	for i, l := range p.p {
+		length := int(l)
+		a.stats.updateState(length, reqScratch)
+
+		g, ok := a.scratch.MakeSegment(length)
+		if !ok {
+			g, ok = a.general.MakeSegment(length)
+		}
+		if !ok {
+			g = a.scratch.GrowAndMakeSegment(length)
+		}
+
+		segs[i] = g
+	}
+
 	return MakeData(segs)
 }
