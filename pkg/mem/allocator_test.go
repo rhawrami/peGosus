72d6e465b1e9f8f0b97bf782b9de979c2fca7695
r h <ravanhawrami@gmail.com>
2026-05-12 18:55:41 -0500

    finish tests for now, FullCoalesce is broken :(

--- pkg/mem/allocator_test.go
+++ pkg/mem/allocator_test.go
@@ -287,12 +287,225 @@ func TestAllocatorDataCache(t *testing.T) {
 	}
 }
 
-func TestAllocatorOptimize(t *testing.T) {}
+func TestAllocatorOptimize(t *testing.T) {
+	nGP, nSP := 10, 5
+	gp, sp := make([]uint64, nGP), make([]uint64, nSP)
+	for i := range gp {
+		gp[i] = rand.Uint64N(50_000)
+	}
+	for i := range sp {
+		sp[i] = rand.Uint64N(25_000)
+	}
 
-func TestAllocatorAllocTemp(t *testing.T) {}
+	a := MakeAllocatorWithProfiles(gp, sp)
 
-func TestAllocatorAllocTempWithProfile(t *testing.T) {}
+	N := 100
+	n := 67
+	d := make([]*Data, N)
+	for i := range N {
+		if i%5 == 0 {
+			d[i] = a.AllocTemp(1_000)
+		} else {
+			d[i] = a.Alloc(5_000)
+		}
+	}
 
-func TestAllocatorAlloc(t *testing.T) {}
+	for i := range n {
+		if i%5 == 0 {
+			continue
+		}
+		d[i].PutAll()
+	}
 
-func TestAllocatorAllocWithProfile(t *testing.T) {}
+	// total hole count should be reduced by some amount after optimizing
+	var h0 uint64
+	for _, v := range a.general.slabs {
+		h0 += v.holes
+	}
+	for _, v := range a.scratch.slabs {
+		h0 += v.holes
+	}
+
+	a.Optimize()
+
+	var h1 uint64
+	for _, v := range a.general.slabs {
+		h1 += v.holes
+	}
+	for _, v := range a.scratch.slabs {
+		h1 += v.holes
+	}
+
+	if h1 >= h0 {
+		t.Errorf("got >= hole count after optimizing: %d and %d", h1, h0)
+	}
+
+}
+
+func TestAllocatorAllocTemp(t *testing.T) {
+	p := uint64(10_000)
+	s := int(p * 2 / 3)
+	gp := []uint64{p}
+	sp := []uint64{p}
+
+	a := MakeAllocatorWithProfiles(gp, sp)
+
+	// first alloc temp should come from scratch
+	d := a.AllocTemp(s)
+	if d.segments[0].slab != a.scratch.slabs[0] {
+		t.Errorf("a1: expected alloc from scratch, but got parent slab %p, expected %p", d.segments[0].slab, a.scratch.slabs[0])
+	}
+	if a.scratch.slabs[0].used == 0 {
+		t.Error("a1: expected alloc from scratch, but used == 0")
+	}
+	if a.stats.nLocs[reqScratch] == 0 {
+		t.Error("a1: expected alloc from scratch, but nLocs scratch == 0")
+	}
+
+	// second alloc temp should come from general
+	d = a.AllocTemp(s)
+	if d.segments[0].slab != a.general.slabs[0] {
+		t.Errorf("a2: expected alloc from general, but got parent slab %p, expected %p", d.segments[0].slab, a.general.slabs[0])
+	}
+	if a.general.slabs[0].used == 0 {
+		t.Error("a2: expected alloc from general, but used == 0")
+	}
+	if a.stats.nLocs[reqScratch] != 2 {
+		t.Error("a2: expected alloc request from scratch, but nLocs scratch != 2")
+	}
+
+	// third alloc temp should cause scratch to grow, and come from scratch
+	d = a.AllocTemp(s)
+	if d.segments[0].slab != a.scratch.slabs[1] {
+		t.Errorf("a3: expected alloc from scratch, but got parent slab %p, expected %p", d.segments[0].slab, a.scratch.slabs[1])
+	}
+	if a.scratch.slabs[1].used == 0 {
+		t.Error("a3: expected alloc from scratch, but used == 0")
+	}
+	if a.stats.nLocs[reqScratch] != 3 {
+		t.Error("a3: expected alloc from scratch, but nLocs scratch != 3 ")
+	}
+
+}
+
+func TestAllocatorAllocTempWithProfile(t *testing.T) {
+	p := uint64(10_240)
+	s := p / 10
+	gp := []uint64{p / 20}
+	sp := []uint64{p}
+
+	a := MakeAllocatorWithProfiles(gp, sp)
+
+	// req 8 segments
+	reqP := []uint64{s, s, s, s, s, s, s, s}
+
+	d := a.AllocTempWithProfile(reqP)
+
+	for i := range d.segments {
+		if d.segments[i] != a.scratch.slabs[0].segments[i] {
+			t.Errorf("on %d: got seg %p, expected %p", i, d.segments[i], a.scratch.slabs[i].segments[i])
+		}
+	}
+	if a.scratch.slabs[0].used != s*uint64(len(reqP)) {
+		t.Errorf("got scratch 0 used %d, expected %d", a.scratch.slabs[0].used, s*uint64(len(reqP)))
+	}
+	if a.stats.nLocs[reqScratch] != uint64(len(reqP)) {
+		t.Errorf("requested from scratch %d times, got %d, expected %d", len(reqP), a.stats.nLocs[reqScratch], len(reqP))
+	}
+
+	// req 4 segments, first 2 should come from scratch 0, last 2 from scratch 1
+	reqP = []uint64{s, s}
+
+	d = a.AllocTempWithProfile(reqP)
+
+	for i := range d.segments {
+		if i < 2 {
+			if d.segments[i].slab != a.scratch.slabs[0] {
+				t.Errorf("on %d, got slab %p, expected slab 0 %p", i, d.segments[i].slab, a.scratch.slabs[0])
+			}
+		}
+		if i > 1 {
+			if d.segments[i].slab != a.scratch.slabs[1] {
+				t.Errorf("on %d, got slab %p, expected slab 1 %p", i, d.segments[i].slab, a.scratch.slabs[1])
+			}
+		}
+	}
+}
+
+func TestAllocatorAlloc(t *testing.T) {
+	p := uint64(10_000)
+	s := int(p * 2 / 3)
+	gp := []uint64{p}
+	sp := []uint64{p}
+
+	a := MakeAllocatorWithProfiles(gp, sp)
+
+	// alloc 1 should come from general 0
+	d := a.Alloc(s)
+	if a.general.slabs[0].used == 0 {
+		t.Error("alloc 1: got slab used of 0")
+	}
+	if d.segments[0].slab != a.general.slabs[0] {
+		t.Errorf("alloc 1: got slab %p, expected %p", d.segments[0].slab, a.general.slabs[0])
+	}
+	if a.stats.nLocs[reqGeneral] != 1 {
+		t.Errorf("alloc 1: got reqGeneral %d, expected %d", a.stats.nLocs[reqGeneral], 1)
+	}
+
+	// alloc 2 should come from general 1
+	d = a.Alloc(s)
+	if a.general.slabs[1].used == 0 {
+		t.Error("alloc 2: got slab used of 0")
+	}
+	if d.segments[0].slab != a.general.slabs[1] {
+		t.Errorf("alloc 2: got slab %p, expected %p", d.segments[0].slab, a.general.slabs[1])
+	}
+	if a.stats.nLocs[reqGeneral] != 2 {
+		t.Errorf("alloc 2: got reqGeneral %d, expected %d", a.stats.nLocs[reqGeneral], 2)
+	}
+}
+
+func TestAllocatorAllocWithProfile(t *testing.T) {
+	p := uint64(10_240)
+	s := p / 10
+	gp := []uint64{p}
+	sp := []uint64{p}
+
+	a := MakeAllocatorWithProfiles(gp, sp)
+
+	// req 8 segments
+	reqP := []uint64{s, s, s, s, s, s, s, s}
+
+	d := a.AllocWithProfile(reqP)
+
+	for i := range d.segments {
+		if d.segments[i] != a.general.slabs[0].segments[i] {
+			t.Errorf("on %d: got seg %p, expected %p", i, d.segments[i], a.general.slabs[i].segments[i])
+		}
+	}
+	if a.general.slabs[0].used != s*uint64(len(reqP)) {
+		t.Errorf("got scratch 0 used %d, expected %d", a.general.slabs[0].used, s*uint64(len(reqP)))
+	}
+	if a.stats.nLocs[reqGeneral] != uint64(len(reqP)) {
+		t.Errorf("requested from general %d times, got %d, expected %d", len(reqP), a.stats.nLocs[reqGeneral], len(reqP))
+	}
+
+	// req 4 segments, first 2 should come from general 0, last 2 from general 1
+	reqP = []uint64{s, s}
+
+	d = a.AllocWithProfile(reqP)
+
+	for i := range d.segments {
+		if i < 2 {
+			if d.segments[i].slab != a.general.slabs[0] {
+				t.Errorf("on %d, got slab %p, expected slab 0 %p", i, d.segments[i].slab, a.general.slabs[0])
+			}
+		}
+		if i > 1 {
+			if d.segments[i].slab != a.general.slabs[1] {
+				t.Errorf("on %d, got slab %p, expected slab 1 %p", i, d.segments[i].slab, a.general.slabs[1])
+			}
+		}
+	}
+
+}
