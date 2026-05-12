e643f097db01aaf9c55807aa1cc687ad2cacb2c7
r h <ravanhawrami@gmail.com>
2026-05-12 00:31:37 -0500

    try uniform type fat struct

--- pkg/array/statistics.go
+++ pkg/array/statistics.go
@@ -1,5 +1,34 @@
 package array
 
+import "math"
+
+// StatU64T represents the unsigned 64-bit integer value of any given statistic.
+type StatU64T uint64
+
+// I64ToStatU64T converts `x` to a StatU64T.
+func I64ToStatU64T(x int64) StatU64T { return StatU64T(x) }
+
+// I64ToStatU64T converts `x` to a StatU64T.
+func I32ToStatU64T(x int32) StatU64T { return StatU64T(x) }
+
+// I64ToStatU64T converts `x` to a StatU64T.
+func F64ToStatU64T(x float64) StatU64T { return StatU64T(math.Float64bits(x)) }
+
+// I64ToStatU64T converts `x` to a StatU64T.
+func F32ToStatU64T(x float32) StatU64T { return StatU64T(math.Float32bits(x)) }
+
+// ToI64T converts `x` to an int64 value.
+func (x StatU64T) ToI64T() int64 { return int64(x) }
+
+// ToI32T converts `x` to an int32 value.
+func (x StatU64T) ToI32T() int32 { return int32(x) }
+
+// ToF64T converts `x` to an float64 value.
+func (x StatU64T) ToF64T() float64 { return math.Float64frombits(uint64(x)) }
+
+// ToF32T converts `x` to an float32 value.
+func (x StatU64T) ToF32T() float32 { return math.Float32frombits(uint32(x)) }
+
 // CachedStatID represents the possible statistics tracked by
 // an array's cached statistics.
 type CachedStatID int
@@ -7,29 +36,29 @@ type CachedStatID int
 const (
 	// maximum value; defined on numeric arrays;
 	// int64 for integer arrays; float64 for floating-point arrays
-	MAX CachedStatID = iota
+	StatMAX CachedStatID = iota
 	// minimum value; defined on numeric arrays;
 	// int64 for integer arrays; float64 for floating-point arrays
-	MIN
+	StatMIN
 	// horizontal sum; defined on numeric arrays;
 	// int64 for integer arrays; float64 for floating-point arrays
-	SUM
+	StatSUM
 	// arithmetic mean; defined on numeric arrays;
 	// float64 for all numeric array types
-	MEAN
+	StatMEAN
 	// standard deviation; defined on numeric arrays;
 	// float64 for all numeric array types
-	STDDEV
+	StatSTDDEV
 	// number of unique values; defined on all arrays;
-	UNIQ
+	StatUNIQ
 	// maximum length; defined on string arrays
-	MAXLEN
+	StatMAXLEN
 	// minimum length; defined on string arrays
-	MINLEN
+	StatMINLEN
 	// array only contains ASCII elements; defined on string arrays
-	ISASCII
+	StatISASCII
 	// array is sorted; defined on all arrays
-	ISSORTED
+	StatISSORTED
 )
 
 // CachedStatistics contain a handful of aggregate statistics
@@ -38,107 +67,32 @@ const (
 // this will mean that an Int64T array will track IsASCII in its statistics, even
 // though it's meaningless.
 type CachedStatistics struct {
-	MaxI64   int64   // bit 0 (defined on integer arrays)
-	MaxF64   float64 // bit 0 (defined on floating-point arrays)
-	MinI64   int64   // bit 1 (defined on integer arrays)
-	MinF64   float64 // bit 1 (defined on floating-point arrays)
-	SumI64   int64   // bit 2 (defined on integer arrays)
-	SumF64   float64 // bit 2 (defined on floating-point arrays)
-	Mean     float64 // bit 3 (defined on numeric arrays)
-	StdDev   float64 // bit 4 (defined on numeric arrays)
-	Uniq     uint64  // bit 5 (defined on all arrays)
-	MaxLen   uint64  // bit 6 (defined on string arrays)
-	MinLen   uint64  // bit 7 (defined on string arrays)
-	IsASCII  bool    // bit 8 (defined on string arrays)
-	IsSorted bool    // bit 9 (defined on all arrays)
-	Validity uint16  // bitmask representing validity of tracked statistics
+	stats    [10]StatU64T // statistics, with offsets corresponding to ID values
+	validity uint16       // bitmask representing validity of tracked statistics
 }
 
 // ClearAll clears the validity of all statistics.
 func (cs *CachedStatistics) ClearAll() {
-	cs.Validity = 0
+	cs.validity = 0
 }
 
-// Clear clears the validity of a given statistic.
-func (cs *CachedStatistics) Clear(id CachedStatID) {
-	switch id {
-	case MAX:
-		cs.Validity &= ^uint16(1)
-	case MIN:
-		cs.Validity &= ^uint16(1 << 1)
-	case SUM:
-		cs.Validity &= ^uint16(1 << 2)
-	case MEAN:
-		cs.Validity &= ^uint16(1 << 3)
-	case STDDEV:
-		cs.Validity &= ^uint16(1 << 4)
-	case UNIQ:
-		cs.Validity &= ^uint16(1 << 5)
-	case MAXLEN:
-		cs.Validity &= ^uint16(1 << 6)
-	case MINLEN:
-		cs.Validity &= ^uint16(1 << 7)
-	case ISASCII:
-		cs.Validity &= ^uint16(1 << 8)
-	case ISSORTED:
-		cs.Validity &= ^uint16(1 << 9)
-	default:
-		//
-	}
+// ClearValidity clears the validity of a given statistic.
+func (cs *CachedStatistics) ClearValidity(id CachedStatID) {
+	cs.validity &= ^uint16(1 << id)
 }
 
-// Set sets the validity of a given statistic.
-func (cs *CachedStatistics) Set(id CachedStatID) {
-	switch id {
-	case MAX:
-		cs.Validity |= uint16(1)
-	case MIN:
-		cs.Validity |= uint16(1 << 1)
-	case SUM:
-		cs.Validity |= uint16(1 << 2)
-	case MEAN:
-		cs.Validity |= uint16(1 << 3)
-	case STDDEV:
-		cs.Validity |= uint16(1 << 4)
-	case UNIQ:
-		cs.Validity |= uint16(1 << 5)
-	case MAXLEN:
-		cs.Validity |= uint16(1 << 6)
-	case MINLEN:
-		cs.Validity |= uint16(1 << 7)
-	case ISASCII:
-		cs.Validity |= uint16(1 << 8)
-	case ISSORTED:
-		cs.Validity |= uint16(1 << 9)
-	default:
-		//
-	}
+// Set sets the value of a given statistic to `v`, and its validity to true.
+func (cs *CachedStatistics) Set(id CachedStatID, v StatU64T) {
+	cs.stats[id] = v
+	cs.SetValidity(id)
 }
 
-// Check checks the validity of a given statistic.
-func (cs *CachedStatistics) Check(id CachedStatID) bool {
-	switch id {
-	case MAX:
-		return cs.Validity&1 == 1
-	case MIN:
-		return (cs.Validity>>1)&1 == 1
-	case SUM:
-		return (cs.Validity>>2)&1 == 1
-	case MEAN:
-		return (cs.Validity>>3)&1 == 1
-	case STDDEV:
-		return (cs.Validity>>4)&1 == 1
-	case UNIQ:
-		return (cs.Validity>>5)&1 == 1
-	case MAXLEN:
-		return (cs.Validity>>6)&1 == 1
-	case MINLEN:
-		return (cs.Validity>>7)&1 == 1
-	case ISASCII:
-		return (cs.Validity>>8)&1 == 1
-	case ISSORTED:
-		return (cs.Validity>>9)&1 == 1
-	default:
-		return false
-	}
+// SetValidity sets the validity of a given statistic.
+func (cs *CachedStatistics) SetValidity(id CachedStatID) {
+	cs.validity |= uint16(1 << id)
+}
+
+// CheckValidity checks the validity of a given statistic.
+func (cs *CachedStatistics) CheckValidity(id CachedStatID) bool {
+	return (cs.validity>>id)&1 == 1
 }
