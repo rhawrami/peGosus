9a268e547b89cdbca0a94af68b2888ef7c0c6a72
r h <ravanhawrami@gmail.com>
2026-05-15 19:34:05 -0500

    impl very basic dtype interface

---
+++ pkg/dtype/type.go
@@ -0,0 +1,44 @@
+package dtype
+
+// TID signifies a data type ID
+type TID int
+
+const (
+	// 32-bit signed integer
+	INT32T TID = iota
+	// 64-bit signed integer
+	INT64T
+	// 32-bit floating-point
+	FLOAT32T
+	// 64-bit floating-point
+	FLOAT64T
+	// 32-bit signed integer
+	DATET
+	// 64-bit signed integer
+	TIMESTAMPTZT
+	// string (contig buff + offset buff)
+	STRT
+	// bitpacked
+	BOOLT
+)
+
+// Type represents a supported data type.
+type Type interface {
+	// ID returns a type's TID.
+	ID() TID
+	// Size1 returns the bit count for storing one element;
+	// returns -1 if variable length.
+	Size1() int
+	// Size8 returns the byte count for storing one element;
+	// returns -1 if variable length or bitpacked.
+	Size8() int
+	// IsNumeric returns true if the underlying storage is numeric.
+	IsNumeric() bool
+	// IsNumericT returns true if the type is numeric (e.g., DateT returns false).
+	IsNumericT() bool
+}
+
+// TypesEq returns true if the types are equal to each other.
+func TypesEq(x, y Type) bool {
+	return x.ID() == y.ID()
+}
---
+++ pkg/dtype/types.go
@@ -0,0 +1,73 @@
+package dtype
+
+// Int32T is a 32-bit signed integer type.
+type Int32T struct{}
+
+func (t *Int32T) ID() TID          { return INT32T }
+func (t *Int32T) Size1() int       { return 32 }
+func (t *Int32T) Size8() int       { return 4 }
+func (t *Int32T) IsNumeric() bool  { return true }
+func (t *Int32T) IsNumericT() bool { return true }
+
+// Int64T is a 64-bit signed integer type.
+type Int64T struct{}
+
+func (t *Int64T) ID() TID          { return INT64T }
+func (t *Int64T) Size1() int       { return 64 }
+func (t *Int64T) Size8() int       { return 8 }
+func (t *Int64T) IsNumeric() bool  { return true }
+func (t *Int64T) IsNumericT() bool { return true }
+
+// Float32T is a 32-bit floating-point type.
+type Float32T struct{}
+
+func (t *Float32T) ID() TID          { return FLOAT32T }
+func (t *Float32T) Size1() int       { return 32 }
+func (t *Float32T) Size8() int       { return 4 }
+func (t *Float32T) IsNumeric() bool  { return true }
+func (t *Float32T) IsNumericT() bool { return true }
+
+// Float64T is a 64-bit floating-point type.
+type Float64T struct{}
+
+func (t *Float64T) ID() TID          { return FLOAT64T }
+func (t *Float64T) Size1() int       { return 64 }
+func (t *Float64T) Size8() int       { return 8 }
+func (t *Float64T) IsNumeric() bool  { return true }
+func (t *Float64T) IsNumericT() bool { return true }
+
+// DateT is a 32-bit signed integer type.
+type DateT struct{}
+
+func (t *DateT) ID() TID          { return DATET }
+func (t *DateT) Size1() int       { return 32 }
+func (t *DateT) Size8() int       { return 4 }
+func (t *DateT) IsNumeric() bool  { return true }
+func (t *DateT) IsNumericT() bool { return false }
+
+// TimestampTZT is a 32-bit signed integer type.
+type TimestampTZT struct{}
+
+func (t *TimestampTZT) ID() TID          { return TIMESTAMPTZT }
+func (t *TimestampTZT) Size1() int       { return 64 }
+func (t *TimestampTZT) Size8() int       { return 8 }
+func (t *TimestampTZT) IsNumeric() bool  { return true }
+func (t *TimestampTZT) IsNumericT() bool { return false }
+
+// StringT is a string type.
+type StringT struct{}
+
+func (t *StringT) ID() TID          { return STRT }
+func (t *StringT) Size1() int       { return -1 }
+func (t *StringT) Size8() int       { return -1 }
+func (t *StringT) IsNumeric() bool  { return false }
+func (t *StringT) IsNumericT() bool { return false }
+
+// BoolT is a boolean type.
+type BoolT struct{}
+
+func (t *BoolT) ID() TID          { return BOOLT }
+func (t *BoolT) Size1() int       { return 1 }
+func (t *BoolT) Size8() int       { return -1 }
+func (t *BoolT) IsNumeric() bool  { return false }
+func (t *BoolT) IsNumericT() bool { return false }
