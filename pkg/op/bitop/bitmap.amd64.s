06cc1b688f0e5729ce7b6d784e3dc6fdcbf07a5b
r h <ravanhawrami@gmail.com>
2026-09-11 22:20:32 -0500

    fix: use correct three operand form for andn ops

--- pkg/op/bitop/bitmap.amd64.s
+++ pkg/op/bitop/bitmap.amd64.s
@@ -74,7 +74,62 @@ TEXT ·bitWiseXorWithPopCount(SB),NOSPLIT,$0-80
 
 // func bitWiseAndNWithPopCount(src1, src2, dst []byte) uint64
 TEXT ·bitWiseAndNWithPopCount(SB),NOSPLIT,$0-80
-    combineAndReturnSum(ANDNQ, ANDNW)
+    MOVQ src1Addr+0(FP), AX
+    MOVQ src2Addr+24(FP), BX
+    MOVQ dstAddr+48(FP), R8
+    MOVQ srcLen+8(FP), CX
+    MOVQ CX, SI
+    XORQ DI, DI
+    SUBQ $16, SI
+    XORQ R13, R13
+    XORQ R9, R9
+    XORQ R10, R10
+
+    TESTQ CX, CX
+    JEQ exitFn
+
+    CMPQ CX, $16
+    JLE tradLoop
+
+vecLoop:
+    MOVQ (AX), R9
+    MOVQ (BX), R10
+    MOVQ 8(AX), R11
+    MOVQ 8(BX), R12
+    ANDNQ R9, R10, R10
+    ANDNQ R11, R12, R12
+    POPCNTQ R10, R9
+    POPCNTQ R12, R11
+    ADDQ R9, R13
+    ADDQ R11, R13
+    MOVQ R10, (R8)
+    MOVQ R12, 8(R8)
+    ADDQ $16, AX
+    ADDQ $16, BX
+    ADDQ $16, R8
+    ADDQ $16, DI
+    CMPQ DI, SI
+    JLT vecLoop
+
+    XORQ R9, R9
+    XORQ R10, R10
+tradLoop:
+    MOVB (AX), R9
+    MOVB (BX), R10
+    ANDNL R9, R10, R10
+    POPCNTW R10, R9
+    ADDQ R9, R13
+    MOVB R10, (R8)
+    ADDQ $1, AX
+    ADDQ $1, BX
+    ADDQ $1, R8
+    ADDQ $1, DI
+    CMPQ DI, CX
+    JLT tradLoop
+
+exitFn:
+    MOVQ R13, cnt+72(FP)
+    RET
     
 // func popCount(src []byte) uint64
 TEXT ·popCount(SB),NOSPLIT,$0-32
